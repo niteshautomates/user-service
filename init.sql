f04330eca03c60f2c4b3e07546c22f64b7c4b0f7
laborant <laborant@labs.iximiuz.com>
2025-05-15 10:53:13 +0000

    feat: change necoded values

--- init.sql
+++ init.sql
@@ -4,21 +4,21 @@ CREATE DATABASE IF NOT EXISTS userdb;
 -- Switch to the database
 USE userdb;
 
-CREATE USER `root`@`%` IDENTIFIED WITH caching_sha2_password BY 'root';
+-- CREATE USER 'root'@'%' IDENTIFIED WITH caching_sha2_password BY 'root';
 
 
 -- Alter the user's authentication method to mysql_native_admin123
-ALTER USER `root`@`%` IDENTIFIED WITH caching_sha2_password BY 'root';
+ALTER USER 'root'@'%' IDENTIFIED WITH caching_sha2_password BY 'root';
 --  Important:  Use '%' to allow connections from any host (including Docker containers).
 --  If you know the specific host, replace '%' with the hostname or IP address.
 --  For example,  'root'@'%'  or 'root'@'my-server-ip'.
 
 -- Grant all privileges on the database.
-GRANT ALL PRIVILEGES ON `userdb` TO `root`@`%`;
+GRANT ALL PRIVILEGES ON userdb.* TO 'root'@'%';
 FLUSH PRIVILEGES;
 
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON `userdb` TO `root`@`%`;
+GRANT SELECT, INSERT, UPDATE, DELETE ON userdb.* TO 'root'@'%';
 FLUSH PRIVILEGES;
 -- Create the users table
 CREATE TABLE IF NOT EXISTS users (
